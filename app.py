bfac00acd0c80906fe41bf080537897c34c6b470
Muhamad Mulyadi <muhamadmulyadi20@gmail.com>
2020-09-08 02:50:28 +0700

    mul123

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from redis import Redis, RedisError
 import os
 import socket
 
-# 12Connect to Redis TST1121
+# 12Connect to Redis 
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
